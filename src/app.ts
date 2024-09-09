4aa806af91977a25b7298dba719804a8e8b4b5e6
João Victor de Oliveira <joaovoliveira2399@gmail.com>
2024-09-09 13:11:21 -0300

    Update RPC server initialization to use dynamic queue name

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,2 @@
 import main from "./bootstrap/init-rpc";
-// This is for example purposes only, in a real application you would use a more specific queue name
-main("rpc_queue", "./prompts", "amqp://localhost:5672")
-  .then(() => {
-    console.log("RPC server started, listening in rpc_queue");
-  })
-  .catch((err) => {
-    console.error("Error starting RPC server: " + err);
-  });
+export default main;
